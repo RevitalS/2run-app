b2bec8ea78cfabfb7995f24d3240ec2b4c6ad7ee
marwaigb <marwa.egbareya.96@gmail.com>
2021-12-08 11:54:20 +0200

    personal details

---
+++ frontend/src/components/PersonalDetailsForm.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfilePicture from "./profilePicture"
+import PersonalDetails from "./profilePicture"
+
+export const PersonalDetailsForm: React.FC= () => {
+
+    return (
+    <>
+    <ProfilePicture/>
+    <PersonalDetails/>
+    </>
+    );
+
+}
---
+++ frontend/src/components/personalDetails.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PesronalDetails: React.FC= () => {
+
+    return (
+      <div>
+          <p>Full name</p>
+          <input type="text" />
+          <p>User name</p>
+          <input type="text" />
+          <p>Full name</p>
+          <input type="text" />
+      </div>
+    );
+  };
+  export default PesronalDetails;
