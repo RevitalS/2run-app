079bcfd24cc07f46836eed9fcfd2c8ac0549d8c9
Revital <revital@Revitals-MacBook-Pro.local>
2021-12-08 12:55:32 +0200

    add flex to matching

--- frontend/src/components/ContractedMatching.tsx
+++ frontend/src/components/ContractedMatching.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
+// import { css, jsx} from '@emotion/react';
+import { css, cx } from '@emotion/css';
 
 const ContractedMatching: React.FC = () => {
+  const color = 'white';
+
+  const styles = css`
+    display: flex;
+    padding: 32px;
+    background-color: hotpink;
+    font-size: 24px;
+    border-radius: 4px;
+    &:hover {
+      color: ${color};
+    };
+    img {
+      border: 1px solid black;
+      border-radius: 100%;
+    }
+    .details {
+      border: 1px solid ${color};
+    }
+    label { 
+      background-color: yellow;
+      margin: 10px;
+    }
+  `;
+
   return (
-    <div>
+    <div className={styles}>
       <img alt='profile' />
-      <div>
-        <label>Name</label>
-        <label>City</label>
+      <div className='details'>
+        <label>Name,</label>
+        <label>City,</label>
         <label>Speed Range</label>
       </div>
     </div>
