482587b5ed5158e2a24cc783f02a1f8d9a3a41c1
Revital <revital@Revitals-MacBook-Pro.local>
2021-12-08 11:12:52 +0200

    add content to contcacted matching

--- frontend/src/components/ContractedMatching.tsx
+++ frontend/src/components/ContractedMatching.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const ContractedMatching: React.FC = () => {
-  return <div>ContractedMatching</div>;
+  return (
+    <div>
+      <img alt='profile' />
+      <div>
+        <label>Name</label>
+        <label>City</label>
+        <label>Speed Range</label>
+      </div>
+    </div>
+  );
 };
 
 export default ContractedMatching;
