f5e1d804ec656b1cf2d60fd7a33b60be73cd2ec3
Revital <revital@Revitals-MacBook-Pro.local>
2021-12-08 18:00:45 +0200

    style layout

--- frontend/src/components/MatchingProfile.tsx
+++ frontend/src/components/MatchingProfile.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { css } from '@emotion/css';
 
-
 const MatchingProfile: React.FC = () => {
   const color = 'white';
 
   const styles = css`
     display: flex;
+    flex-wrap: wrap;
     padding: 32px;
     background-color: #41969c;
     font-size: 24px;
@@ -22,19 +22,40 @@ const MatchingProfile: React.FC = () => {
       height: 100px;
       width: 100px;
     }
-    .details {
+    .col {
       display: flex;
       flex-wrap: wrap;
+      flex-direction: column;
+      grid-area: main;
     }
     label {
       margin: 5px;
     }
+    button {
+      width: 100px;
+      height: 50px;
+    }
   `;
 
   return (
+    <>
     <div className={styles}>
-      MatchingProfile
+        <img />
+        <div className='col'>
+          <label>Name</label>
+          <label>gender</label>
+          <label>age</label>
+          <label>city</label>
+        </div>
+        <div className='col'>
+          <label>speed</label>
+          <label>joggingLevel</label>
+          <label>runningGoals</label>
+        </div>
+        <button>Send Message</button>
     </div>
+    <p>about</p>
+    </>
   );
 };
 
