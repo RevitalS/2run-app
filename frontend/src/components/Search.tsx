5abf445ec44e333844c62234374caacb4e6a6406
Revital <revital@Revitals-MacBook-Pro.local>
2021-12-08 16:09:53 +0200

    add search input

--- frontend/src/components/Search.tsx
+++ frontend/src/components/Search.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Search: React.FC = () => {
-  return <div>Search</div>;
+  return (
+    <div>
+    <label>Serach</label>
+    <input type='text' id='serach' placeholder='Seach a Name' name='Serach'/>
+    </div>
+  );
 };
 
 export default Search;
