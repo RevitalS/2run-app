dbd7fe514d5b91cc149797f3c6c6738302906931
Revital <revital@Revitals-MacBook-Pro.local>
2021-12-09 11:32:13 +0200

    clean inbox component

--- frontend/src/components/Inbox.tsx
+++ frontend/src/components/Inbox.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import ContractedMatching from './ContractedMatching';
-import { City } from '../models/Enums';
-import ContractedMessege from './ContractedMessege';
 import { Outlet } from 'react-router-dom';
 
 const Inbox: React.FC = () => {
