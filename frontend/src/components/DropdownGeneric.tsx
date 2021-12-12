9c312ee29fce0e4fb8d6ec4975d0f66a42cd50bb
Revital <revital@lirans-MBP.meetinplace>
2021-12-12 12:07:39 +0200

    add isRequired prop to dropdown generic

--- frontend/src/components/DropdownGeneric.tsx
+++ frontend/src/components/DropdownGeneric.tsx
@@ -8,15 +8,16 @@ interface IProps {
     handleChange: Function;
     type: string;
     options: Array<string>;
+    isRequired: boolean;
     }
 }
 
 const DropdownGeneric: React.FC<IProps> = (props) => {
-    const {handleChange, type, options} = props.dropdownValues;
+    const {handleChange, type, options, isRequired} = props.dropdownValues;
   return (
       <>
       <label htmlFor={type}>{type}</label>
-      <select onChange={handleChange(type)} required name={type} id={type} >
+      <select onChange={handleChange(type)} required={isRequired} name={type} id={type} >
           {options.map((option, i) => <option key={i} value={option}>{option}</option>)}
       </select>
       </>
